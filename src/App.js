affee1dc256d649056183ec4f07478387d4a45cd
himoura <fallingout31@gmail.com>
2022-04-17 09:56:08 -0400

    coded routes, navLinks, dynamic routes/params

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { Route } from 'react-router-dom';
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
+import { Route, Switch } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
